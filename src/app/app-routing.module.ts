f6ce1188372e6be5e79619ed111623e09998d573
bryanbryan92 <breinah@unal.edu.co>
2021-11-19 17:00:23 -0500

    creando header, footer, y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TablaUsuariosComponent } from './components/tabla-usuarios/tabla-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'usuarios', component: TablaUsuariosComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
